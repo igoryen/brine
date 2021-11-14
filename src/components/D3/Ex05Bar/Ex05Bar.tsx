050ae96dfd373b456452d98bced7acd77807a4bb
igoryen <igor.yentaltsev@gmail.com>
2021-11-14 16:13:58 -0500

    Ex05Bar: added bars

--- src/components/D3/Ex05Bar/Ex05Bar.tsx
+++ src/components/D3/Ex05Bar/Ex05Bar.tsx
@@ -24,7 +24,26 @@ const Ex05Bar = () /* or ( props : IEx05BarProps ) */ => {
   })
 
   const draw = () => {
-    
+    const w = 300;
+    const h = 120;
+    const padding = 2;
+    const dataset = [ 5, 10, 13, 19, 21, 25, 11, 25, 22, 18, 7];
+    const svg = d3.select(ref.current).append('svg')
+      .attr('width', w)
+      .attr('height', h);
+
+    const colorPicker = (value: number) => (value <= 20) ? '#4d94ff' : '#ff0033';
+
+    svg.selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i: number) => i * (w / dataset.length))
+      .attr('y', (d: number) => h - (d*4))
+      .attr('width', w / dataset.length - padding)
+      .attr('height', (d: number) => d*4)
+      .attr('fill', (d: number) => colorPicker(d))
+
   }
   return (
     <div className="learnD3 Ex05Bar" ref={ref} />
