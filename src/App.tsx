a2d123f80451674953aacd2d1d7a974fbe56c0d2
igoryen <igor.yentaltsev@gmail.com>
2021-11-14 18:52:00 -0500

    Ex06Line: fail due to Udemy crap course

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import Ex02Circles from './components/D3/Ex02Circles/Ex02Circles'
 import Ex03Text from './components/D3/Ex03Text/Ex03Text'
 import Ex04Bar from './components/D3/Ex04Bar/Ex04Bar'
 import Ex05Bar from './components/D3/Ex05Bar/Ex05Bar'
+import Ex06Line from './components/D3/Ex06Line/Ex06Line'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Ex06Line />
         <Ex05Bar />
         <Ex04Bar />
         <Ex03Text />
--- src/components/D3/Ex06Line/Ex06Line.tsx
+++ src/components/D3/Ex06Line/Ex06Line.tsx
@@ -9,9 +9,11 @@ $ npx generate-react-cli component Ex06Line --type=d3
 
 */
 
-import React, { 
-  // useState, 
-  useEffect, RefObject } from 'react'
+import React, {
+  // useState,
+  useEffect,
+  RefObject,
+} from 'react'
 import './Ex06Line.scss'
 import * as d3 from 'd3' // yarn add d3 @types/d3
 
@@ -24,12 +26,14 @@ const Ex06Line = () /* or ( props : IEx06LineProps ) */ => {
   })
 
   const draw = () => {
-   
+    // the crap presented on Udemy
+    // in D3.js Data Visualization Fundamentals - Hands On
+    // in video 13. DEMO: Building A Scatter Plot
+    // is outdated and confusing
+
   }
 
-  return (
-    <div className="learnD3 Ex06Line" ref={ref} />
-  )
+  return <div className="learnD3 Ex06Line" ref={ref} />
 }
 
 /*
