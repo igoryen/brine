4e787eb65e31eeb3a857835a9ec061301684e4a9
igoryen <igor.yentaltsev@gmail.com>
2021-11-14 22:49:32 -0500

    Bad code but no TS complaint

--- src/components/TS/Sec02Basics/Sec02Basics.tsx
+++ src/components/TS/Sec02Basics/Sec02Basics.tsx
@@ -11,10 +11,19 @@ const person = {
   name: "Igor",
   age: 40,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'] // a tuple (2 elements)
 };
+
+person.role.push('admin'); // 1
+person.role[1] = 10; // 1
 
 let favouriteActivities: string[];
 
-console.log(person.name)
+console.log(person.name);
+console.log(person)
 
 export default Sec02Basics;
+
+/**
+ * 1. no TS complaint
+ */
