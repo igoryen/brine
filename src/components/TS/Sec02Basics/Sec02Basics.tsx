c3bc5c763fb2893abf34222f814cefd0191d876f
igoryen <igor.yentaltsev@gmail.com>
2021-11-16 00:56:24 -0500

    25. Type aliases & object types. C

--- src/components/TS/Sec02Basics/Sec02Basics.tsx
+++ src/components/TS/Sec02Basics/Sec02Basics.tsx
@@ -55,11 +55,14 @@ const u1: User = { name: 'Max', age: 30 }; // this works!
 console.log("u1:", u1);
 
 
-function greet(user: { name: string; age: number }) {
+type Guest = { name: string; age: number };
+
+function greet(user: Guest) {
   console.log(`Hi, I am ${  user.name}`);
 }
- 
-function isOlder(user: { name: string; age: number }, checkAge: number) {
+greet({name:'Igor', age: 46});
+
+function isOlder(user: Guest, checkAge: number) {
   return checkAge > user.age;
 }
 console.log('isOlder():', isOlder({ name: 'Igor', age: 46 }, 44));
